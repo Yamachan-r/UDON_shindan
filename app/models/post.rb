494312cb4c3398406d12292abd4acc3f36d9d523
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-09-11 23:29:50 +0900

    スタイル変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   validates :number, presence: true, uniqueness: true
+  validates :content, presence: true, uniqueness: true
+  validates :region, presence: true, uniqueness: true
+  validates :info, presence: true, uniqueness: true
 end
