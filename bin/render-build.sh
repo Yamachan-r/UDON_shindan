c84d1d63e50d349f547c94e4078209d4e1a862d8
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-09-13 01:39:10 +0900

    seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,4 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate RAILS_ENV=production
-bundle exec rake db:seed RAILS_ENV=production
+bundle exec rails db:seed RAILS_ENV=production
