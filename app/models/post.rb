568ba47c5031a77c9bf88826e2c9ff4311b5d326
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-09-12 21:23:19 +0900

    result

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ApplicationRecord
   validates :number, presence: true, uniqueness: true
   validates :content, presence: true, uniqueness: true
   validates :region, presence: true, uniqueness: true
-  validates :info, presence: true, uniqueness: true
 end
