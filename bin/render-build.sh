b6232f36b2fcf2efb916245583b66f4563d68a4d
Yamachan-r <r.chun.d.xxx@gmail.com>
2024-09-13 01:07:45 +0900

    環境変数

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake db:seed RAILS_ENV=production
